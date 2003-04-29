96a194663e3cf0e2cbe8075dab61262745493454
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2003-04-29 16:37:23 +0000

    Added MPI_FOPen as well as MPI_GetSize so that mpi exits cleanly when files are not found.

--- main/mympi.c
+++ main/mympi.c
@@ -6,10 +6,14 @@
  * --------------------------------
  * This file contains mpi-based functions.
  *
- * $Id: mympi.c,v 1.1 2002-11-03 00:21:16 fringer Exp $
+ * $Id: mympi.c,v 1.2 2003-04-29 16:37:23 fringer Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2002/11/03 00:21:16  fringer
+ * Initial revision
+ *
  *
  */
+#include <errno.h>
 #include "mympi.h"
 
 /*
@@ -62,7 +66,7 @@ REAL MPI_GetValue(char *file, char *str, char *call, int myproc)
       printf("Couldn't find value for %s!\n",str);
     }
     MPI_Finalize();
-    exit(0);
+    exit(EXIT_FAILURE);
   }
 
   return val;
@@ -79,6 +83,49 @@ void MPI_GetString(char *string, char *file, char *str, char *call, int myproc)
       printf("Couldn't find string for %s!\n",str);
     }
     MPI_Finalize();
-    exit(0);
+    exit(EXIT_FAILURE);
   }
+}
+
+/*
+ * Function: MPI_FOpen
+ * Usage: fid = MPI_FOpen(string,"r","GetValue",myproc);
+ * -----------------------------------------------------
+ * Exits if the requested file does not exist and closes
+ * MPI cleanly. The third string is useful for determining which
+ * function the function was called from.  When two processes
+ * are trying to read the same file at the same time, an error
+ * code of EAGAIN results.  This is ommitted as a possible error
+ * code.
+ *
+ */
+FILE *MPI_FOpen(char *file, char *perms, char *caller, int myproc) {
+  extern int errno;
+  char str[BUFFERLENGTH];
+  FILE *fid = fopen(file,perms);
+
+  if(errno && errno!=EAGAIN) {
+    if(myproc==0) {
+      sprintf(str,"Error in Function %s while trying to open %s",caller,file);
+      perror(str);
+    }
+    MPI_Finalize();
+    exit(EXIT_FAILURE);
+  } else
+    return fid;
+}
+
+/*
+ * Function: MPI_GetSize
+ * Usage: N = MPI_GetSize(filename,"ReadMainGrid",myproc);
+ * -------------------------------------------------------
+ * Uses the getsize function defined in fileio.c but checks
+ * for file existence first.
+ *
+ */
+int MPI_GetSize(char *file, char *caller, int myproc) {
+  FILE *fid = MPI_FOpen(file,"r",caller,myproc);
+  fclose(fid);
+
+  return getsize(file);
 }
