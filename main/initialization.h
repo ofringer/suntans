d8144c4271143984ad844efa702f0ad2020e448f
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2003-05-12 00:19:51 +0000

    Added REAL d to the ReturnFreeSurface function so that the depth can be used to initialize the free surface

--- main/initialization.h
+++ main/initialization.h
@@ -2,15 +2,18 @@
  * File: initialization.h
  * Description: Header file for initialization.c
  *
- * $Id: initialization.h,v 1.1 2003-04-29 00:23:33 fringer Exp $
+ * $Id: initialization.h,v 1.2 2003-05-12 00:19:51 fringer Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2003/04/29 00:23:33  fringer
+ * Initial revision
+ *
  *
  */
 #ifndef _initialization_h
 #define _initialization_h
 
 REAL ReturnDepth(REAL x, REAL y);
-REAL ReturnFreeSurface(REAL x, REAL y);
+REAL ReturnFreeSurface(REAL x, REAL y, REAL d);
 REAL ReturnSalinity(REAL x, REAL y, REAL z);
 REAL ReturnHorizontalVelocity(REAL x, REAL y, REAL n1, REAL n2, REAL z);
 
