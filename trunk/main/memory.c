0e1b987e1e19eeb8c035f9e216c6d1c7c112253d
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2003-06-10 02:22:36 +0000

    Updated to print out memory stats if VerboseMemory==1.

--- trunk/main/memory.c
+++ trunk/main/memory.c
@@ -7,60 +7,65 @@
  * is set to 1, then memory statistics will be printed as memory
  * is allocated and freed.
  *
- * $Id: memory.c,v 1.1 2003-04-29 00:10:32 fringer Exp $
+ * $Id: memory.c,v 1.2 2003-06-10 02:22:36 fringer Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2003/04/29 00:10:32  fringer
+ * Initial revision
+ *
  *
  */
 #include<stdlib.h>
 #include "memory.h"
 
 /*
- * Function: MyMalloc
- * Usage: ptr=(int *)MyMalloc(N*sizeof(int));
- * ------------------------------------------
+ * Function: SunMalloc
+ * Usage: ptr=(int *)SunMalloc(N*sizeof(int),"Function");
+ * ------------------------------------------------------
  * Same as the malloc function in stdlib.h, but this
  * one keeps track of the total memory with the global
  * variable TotSpace.
  *
  */
-void *MyMalloc(const int bytes) {
+void *SunMalloc(const int bytes, const char *function) {
   void *ptr = malloc(bytes);
+
+  //VerboseMemory=1;
 
   if(TotSpace==-1)
     TotSpace=0;
 
   if(ptr==NULL) {
     printf("Error.  Out of memory!\n");
-    printf("Total memory: %d, attempted to allocate: %d\n",
-	   TotSpace,bytes);
+    printf("Total memory: %d, attempted to allocate: %d in function %s\n",
+	   TotSpace,bytes,function);
     exit(1);
   } else {
     TotSpace+=bytes;
     if(VerboseMemory)
-      printf("Allocated %d, Total: %d\n",
-	     bytes,TotSpace);
+      printf("Allocated %d, Total: %d (%s)\n",
+	     bytes,TotSpace,function);
     return ptr;
   }
 }
 
 /*
- * Function: MyFree
- * Usage: MyFree(ptr);
+ * Function: SunFree
+ * Usage: SunFree(ptr,bytes,"Function");
  * ------------------------------------------
  * Same as the free function in stdlib.h, but this
  * one keeps track of the total memory with the global
  * variable TotSpace.
  *
  */
-void MyFree(void *ptr, int bytes) {
+void SunFree(void *ptr, const int bytes, const char *function) {
   if(ptr==NULL) {
-    printf("Error!  Attempting to free a NULL pointer!\n");
+    printf("Error!  Attempting to free a NULL pointer in funciton %s\n",function);
     exit(1);
   } else {
     free(ptr);
     TotSpace-=bytes;
     if(VerboseMemory)
-      printf("Freed %d, Total: %d\n",
-	     bytes,TotSpace);
+      printf("Freed %d, Total: %d (%s)\n",
+	     bytes,TotSpace,function);
   }
 }
