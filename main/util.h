f02afd84d546d767370f24e370bdff9b0c8c1643
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2004-06-15 18:36:33 +0000

    Changed definition of Max to static.

--- main/util.h
+++ main/util.h
@@ -1,8 +1,11 @@
 /*
  * Header file for util.c
  *
- * $Id: util.h,v 1.3 2004-05-29 20:25:02 fringer Exp $
+ * $Id: util.h,v 1.4 2004-06-15 18:36:33 fringer Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2004/05/29 20:25:02  fringer
+ *  Revision before converting to CVS.
+ *
  * Revision 1.2  2002/11/05 01:31:17  fringer
  * Added baroclinic term
  *
@@ -28,6 +31,6 @@ void TriSolve(REAL *a, REAL *b, REAL *c, REAL *d, REAL *u, int N);
 int IsNan(REAL x);
 REAL UpWind(REAL u, REAL dz1, REAL dz2);
 void Copy(REAL **from, REAL **to, gridT *grid);
-REAL Max(REAL x1, REAL x2);
+static REAL Max(REAL x1, REAL x2);
 
 #endif
