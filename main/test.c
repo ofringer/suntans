a500d066a51ab78edf6ccac8fd9a93f1e57b298d
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2004-06-23 05:29:15 +0000

    Changing to test list of writers.

--- main/test.c
+++ main/test.c
@@ -2,8 +2,11 @@
  * file: test.c
  * ------------
  *
- * $Id: test.c,v 1.2 2004-05-29 20:25:02 fringer Exp $
+ * $Id: test.c,v 1.3 2004-06-23 05:29:15 fringer Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/05/29 20:25:02  fringer
+ *  Revision before converting to CVS.
+ *
  * Revision 1.1  2003/07/31 09:01:09  fringer
  * Initial revision
  *
@@ -16,4 +19,5 @@ int main(void) {
   
   int *a = (int *)MyMalloc(20);
   MyFree(a,20);
+
 }
