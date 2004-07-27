e02ce4fae1f80e825cfebeb6a34fdfb3f46b28dc
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2004-07-27 20:36:28 +0000

    Added SetBoundaryScalars function prototype.

--- trunk/main/boundaries.h
+++ trunk/main/boundaries.h
@@ -1,8 +1,11 @@
 /*
  * Header file for boundaries.c
  *
- * $Id: boundaries.h,v 1.2 2004-05-29 20:25:02 fringer Exp $
+ * $Id: boundaries.h,v 1.3 2004-07-27 20:36:28 fringer Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/05/29 20:25:02  fringer
+ * Revision before converting to CVS.
+ *
  * Revision 1.1  2004/03/12 06:22:13  fringer
  * Initial revision
  *
@@ -16,5 +19,6 @@
 #include "grid.h"
 
 void OpenBoundaryFluxes(REAL **q, REAL **ub, REAL **ubn, gridT *grid, physT *phys, propT *prop);
+void SetBoundaryScalars(REAL **boundary_scal, gridT *grid, physT *phys, propT *prop, char *type);
 
 #endif
