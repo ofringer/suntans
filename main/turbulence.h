5f87d73851b1a80a1eb23e4d492f9c4840c27717
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2004-09-13 04:14:55 +0000

    Header file for turbulence.c.

---
+++ main/turbulence.h
@@ -0,0 +1,14 @@
+/*
+ * File: turbulence.h
+ * Description: Header file for turbulence.c
+ *
+ * $Id: turbulence.h,v 1.1 2004-09-13 04:14:55 fringer Exp $
+ * $Log: not supported by cvs2svn $
+ *
+ */
+#ifndef _turbulence_h
+#define _turbulence_h
+
+void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q, REAL **Cn_l, REAL **nuT, REAL **kappaT);
+
+#endif
