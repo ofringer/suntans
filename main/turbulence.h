84dd063635f60190a239d99bc230c47cf5ae2687
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2004-09-16 21:33:01 +0000

    Added comm and myproc variables to definition of my25.

--- main/turbulence.h
+++ main/turbulence.h
@@ -2,13 +2,19 @@
  * File: turbulence.h
  * Description: Header file for turbulence.c
  *
- * $Id: turbulence.h,v 1.1 2004-09-13 04:14:55 fringer Exp $
+ * $Id: turbulence.h,v 1.2 2004-09-16 21:33:01 fringer Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2004/09/13 04:14:55  fringer
+ * Header file for turbulence.c.
+ *
  *
  */
 #ifndef _turbulence_h
 #define _turbulence_h
 
-void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q, REAL **Cn_l, REAL **nuT, REAL **kappaT);
+// Von Karman's Constant
+#define KAPPA_VK 0.42
+
+void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q, REAL **Cn_l, REAL **nuT, REAL **kappaT, MPI_Comm comm, int myproc);
 
 #endif
