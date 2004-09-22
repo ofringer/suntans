c9dffb7b91cd6913fdcf569a019a45912490c864
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2004-09-22 06:29:33 +0000

    Latest version of my25 function with several changes.  The previous version was checked in too soon and was not yet working properly.

--- main/turbulence.c
+++ main/turbulence.c
@@ -2,8 +2,14 @@
  * File: turbulence.c
  * Description:  Contains the Mellor-Yamad level 2.5 turbulence model.
  *
- * $Id: turbulence.c,v 1.2 2004-09-15 01:10:47 fringer Exp $
+ * $Id: turbulence.c,v 1.3 2004-09-22 06:29:33 fringer Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/09/15 01:10:47  fringer
+ * Added MY25 but it seems to have problems predicting the log layer
+ * correctly for a channel flow.  For some reason the velocity is
+ * underpredicted by the code because the eddy viscosity is overpredicted
+ * when compared to its theoretical value.
+ *
  * Revision 1.1  2004/09/13 04:14:36  fringer
  * Contains functions that compute the eddy viscosity and scalar
  * diffusivity.
@@ -30,17 +36,18 @@ FILE *fid;
  * quantities q^2 and q^2l is included with the use of UpdateScalars
  *
  */
-void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q, REAL **Cn_l, REAL **nuT, REAL **kappaT) {
-  int i, j, iptr, k, nf, iplot=89, nc1, nc2;
-  REAL thetaQ=0.75, CdAvgT, CdAvgB, *dudz, *dvdz, *drdz, z, *N, *Gh, z0;
-  REAL A1, A2, B1, B2, C1, E1, E2, E3, Sq, Sm, Sh, kappa_vk;
+void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q, REAL **Cn_l, REAL **nuT, REAL **kappaT, MPI_Comm comm, int myproc) {
+  int i, j, iptr, k, nf, iplot=10, nc1, nc2;
+  REAL thetaQ=0.75, CdAvgT, CdAvgB, *dudz, *dvdz, *drdz, z, *N, *Gh;
+  REAL A1, A2, B1, B2, C1, E1, E2, E3, Sq, Sm, Sh;
 
-  if(prop->n==1)
+  if(prop->n==prop->nstart+1)
     fid=fopen("/tmp/turb.dat","w");
 
   N = dudz = phys->a;
-  Gh = dvdz = phys->b;
+  dvdz = phys->b;
   drdz = phys->c;
+  Gh = phys->d;
 
   // Specification of constants
   A1 = 0.92;
@@ -52,31 +59,7 @@ void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q
   E2 = 1.33;
   E3 = 0.25;
   Sq = 0.2;
-  kappa_vk = 0.42;
-  z0 = 0.0025;
   
-  if(prop->n==1) {
-    for(j=0;j<grid->Ne;j++) {
-      nc1=grid->grad[2*j];
-      nc2=grid->grad[2*j+1];
-      if(nc1==-1) nc1=nc2; 
-      if(nc2==-1) nc2=nc1; 
-      if(grid->Nk[nc2]>grid->Nk[nc1]) nc1=nc2;
-      if(grid->Nk[nc1]>grid->Nk[nc2]) nc2=nc1;
-
-      phys->CdT[j]=pow(log(0.5*(grid->dzz[nc1][grid->ctop[nc1]]+grid->dzz[nc2][grid->ctop[nc2]])/z0)/kappa_vk,-2);
-      phys->CdB[j]=pow(log(0.5*(grid->dzz[nc1][grid->Nk[nc1]-1]+grid->dzz[nc2][grid->Nk[nc2]-1])/z0)/kappa_vk,-2);
-    }
-
-    for(i=0;i<grid->Nc;i++) 
-      for(k=0;k<grid->Nk[i];k++) {
-	nuT[i][k]=0;
-	kappaT[i][k]=0;
-	q[i][k]=0;
-	l[i][k]=0;
-      }
-  }
-
   // First solve for q^2 and store its old value in stmp3
   for(iptr=grid->celldist[0];iptr<grid->celldist[1];iptr++) {
     i=grid->cellp[iptr];
@@ -94,12 +77,12 @@ void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q
     dvdz[grid->Nk[i]]=dvdz[grid->Nk[i]-1];
     drdz[grid->Nk[i]]=drdz[grid->Nk[i]-1];
 
-    // qtmp will store src1 for q^2, which is the 2q/B1 l term
+    // uold will store src1 for q^2, which is the 2q/B1 l term
     // wtmp will store src2 for q^2, which is the 2 (Ps+Pb) term
     for(k=grid->ctop[i];k<grid->Nk[i];k++) {
-      phys->qtmp[i][k]=2.0*q[i][k]/B1/(l[i][k]+SMALL);
+      phys->uold[i][k]=2.0*q[i][k]/B1/(l[i][k]+SMALL);
       phys->wtmp[i][k]=2.0*fabs((prop->nu+nuT[i][k])*(pow(0.5*(dudz[k]+dudz[k+1]),2)+pow(0.5*(dvdz[k]+dvdz[k+1]),2))+
-				GRAV*(prop->kappa_s+kappaT[i][k])*0.5*(drdz[k]+drdz[k+1]));
+      				GRAV*(prop->kappa_s+kappaT[i][k])*0.5*(drdz[k]+drdz[k+1]));
     }
 
     // kappaT will store the diffusion coefficient for q^2
@@ -121,23 +104,23 @@ void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q
       CdAvgT+=phys->CdT[grid->face[i*NFACES+nf]]/3;
       CdAvgB+=phys->CdB[grid->face[i*NFACES+nf]]/3;
     }
-
     phys->htmp[i]=pow(B1,2/3)*CdAvgT*pow(phys->uc[i][grid->ctop[i]],2)+pow(phys->vc[i][grid->ctop[i]],2);
     phys->hold[i]=pow(B1,2/3)*CdAvgB*pow(phys->uc[i][grid->Nk[i]-1],2)+pow(phys->vc[i][grid->Nk[i]-1],2);
   }
-  UpdateScalars(grid,phys,prop,q,NULL,phys->Cn_q,0,0,kappaT,thetaQ,phys->qtmp,phys->wtmp,phys->htmp,phys->hold,1,1);
+  UpdateScalars(grid,phys,prop,q,NULL,phys->Cn_q,0,0,kappaT,thetaQ,phys->uold,phys->wtmp,phys->htmp,phys->hold,1,1);
 
   // q now contains q^2
   for(iptr=grid->celldist[0];iptr<grid->celldist[1];iptr++) {
     i=grid->cellp[iptr];
 
-    // qtmp will store src1 for q^2 l, which is the q/B1 l*(1+E2(l/kz)^2+E3(l/k(H-z))^2) term
+    // uold will store src1 for q^2 l, which is the q/B1 l*(1+E2(l/kz)^2+E3(l/k(H-z))^2) term
     // wtmp will store src2 for q^2 l, which is the l E1 (Ps+Pb) term
-    z=grid->dzz[i][grid->ctop[i]]/2;
+    z = phys->h[iplot];
     for(k=grid->ctop[i];k<grid->Nk[i];k++) {
-      phys->qtmp[i][k]*=0.5*(1+E2*pow(l[i][k]/kappa_vk/(grid->dv[i]+phys->h[i]-z),2)+E3*pow(l[i][k]/kappa_vk/z,2));
+      z-=grid->dzz[i][k]/2;
+      phys->uold[i][k]*=0.5*(1+E2*pow(l[i][k]/KAPPA_VK/(z-phys->h[i]),2)+E3*pow(l[i][k]/KAPPA_VK/(grid->dv[i]+z),2));
       phys->wtmp[i][k]*=0.5*l[i][k]*E1;
-      z+=grid->dzz[i][k];
+      z-=grid->dzz[i][k]/2;
     }
 
     // kappaT already stores q l Sq from before
@@ -155,18 +138,22 @@ void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q
     for(iptr=grid->celldist[0];iptr<grid->celldist[1];iptr++) {
       i=grid->cellp[iptr];
       
-      for(k=grid->ctop[i];k<grid->Nk[i];k++) 
+      for(k=grid->ctop[i];k<grid->Nk[i];k++) {
 	l[i][k]+=prop->dt*pow(q[i][k],3/2)/B1;
+      }
     }
   else
-    UpdateScalars(grid,phys,prop,l,NULL,phys->Cn_l,0,0,kappaT,thetaQ,phys->qtmp,phys->wtmp,phys->htmp,phys->hold,1,1);
+    UpdateScalars(grid,phys,prop,l,NULL,phys->Cn_l,0,0,kappaT,thetaQ,phys->uold,phys->wtmp,phys->htmp,phys->hold,1,1);
+
+  // Send/Recv q and l data to neighboring processors
+  ISendRecvCellData3D(q,grid,myproc,comm);
+  ISendRecvCellData3D(l,grid,myproc,comm);
 
   // l stores q^2 l
   // q stores q^2
   // Extract q and l from their stored quantities
   // and then set the values of nuT and kappaT
-  for(iptr=grid->celldist[0];iptr<grid->celldist[1];iptr++) {
-    i=grid->cellp[iptr];
+  for(i=0;i<grid->Nc;i++) {
 
     for(k=grid->ctop[i]+1;k<grid->Nk[i];k++) {
       drdz[k]=-2.0*GRAV*prop->beta*(phys->s[i][k-1]-phys->s[i][k])/(grid->dzz[i][k-1]+grid->dzz[i][k]);
@@ -190,9 +177,13 @@ void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q
     }
   }
 
-  if(!(prop->n%prop->ntout) && prop->n>1) {
-    for(k=grid->ctop[iplot];k<grid->Nk[iplot];k++) 
-      fprintf(fid,"%e %e %e %e %e\n",phys->uc[iplot][k],nuT[iplot][k],kappaT[iplot][k],q[iplot][k],l[iplot][k]);
+  if(!(prop->n%prop->ntout) || prop->n==1) {
+    z=phys->h[iplot];
+    for(k=grid->ctop[iplot];k<grid->Nk[iplot];k++) {
+      z-=grid->dzz[iplot][k]/2;
+      fprintf(fid,"%f %e %e %e %e %e\n",z,phys->uc[iplot][k],nuT[iplot][k],kappaT[iplot][k],q[iplot][k],l[iplot][k]);
+      z-=grid->dzz[iplot][k]/2;
+    }
     fflush(fid);
   }
 }
