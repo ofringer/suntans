7733eb487cc74d528b92d45e3b18076bef2721e8
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2005-07-12 01:27:14 +0000

    Examples subdirectory added.  Contains tests for: 1) Time accuracy 2) Lock exchange 3) Wetting and drying 4) Boundary condition (river plume) 5) Internal waves

---
+++ trunk/main/examples/lockexchange/lockexchange.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+########################################################################
+#
+# Shell script to run a suntans test case.
+#
+########################################################################
+
+SUNTANSHOME=../..
+SUN=$SUNTANSHOME/sun
+SUNPLOT=$SUNTANSHOME/sunplot
+
+maindatadir=rundata
+datadir=data
+
+NUMPROCS=$1
+
+if [ ! -d $datadir ] ; then
+    cp -r $maindatadir $datadir
+    echo Creating grid...
+    mpirun -np $NUMPROCS $SUN -t -g --datadir=$datadir
+else
+    cp $maindatadir/suntans.dat $datadir/.
+fi
+
+echo Running suntans...
+mpirun -np $NUMPROCS $SUN -s -vv --datadir=$datadir
