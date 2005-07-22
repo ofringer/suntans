fba841cde965c4140517bbe1ffb03fa57b0d4b43
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2005-07-22 02:22:00 +0000

    Finished boundaries example section.

---
+++ papers/guide/figures/plumegrid.m
@@ -0,0 +1,20 @@
+p = load('plumegrid/points.dat');
+c = load('plumegrid/cells.dat');
+e = load('plumegrid/edges.dat');
+
+x = p(:,1)/1000;
+y = p(:,2)/1000;
+ip = 1+c(:,[3:5,3]);
+ie = 1+e(:,[1,2]);
+ig = find(e(:,3)==2);
+
+
+figure(1);
+clf;
+hold on;
+plot(x(ip)',y(ip)','k-');
+plot(x(ie(ig,:))',y(ie(ig,:))','k-','linewidth',5);
+
+axis image;
+xlabel('x (km)')
+ylabel('y (km)');
