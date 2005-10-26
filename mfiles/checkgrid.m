f99b26457710ea6b06ed1107cb217e8b233c0eef
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2005-10-26 15:29:48 +0000

    Only displays a plot if dgmin is nonzero.

--- mfiles/checkgrid.m
+++ mfiles/checkgrid.m
@@ -31,22 +31,26 @@ function checkgrid(dgmin,nbins,edgesfile,cellsfile)
   g2jp1(find(g2jp1==0))=[];
 
   Dg = sqrt((xv(g2j)-xv(g2jp1)).^2+(yv(g2j)-yv(g2jp1)).^2);
-  fprintf('Minimum Dg is %.2f\n',min(Dg));
-  ind = find(Dg<dgmin);
-
-  figure(1);
-  hist(Dg,linspace(min(Dg),max(Dg),nbins));
-  xlabel('Voronoi Distance');
-  ylabel('Number of edges');
-  title('Voronoi Distance Histogram');
-
-  figure(2);
-  clf;
-  hold on;
-  plot(xv,yv,'k.','markersize',1);
-  plot([xv(g2j(ind)),xv(g2jp1(ind))]',...
-       [yv(g2j(ind)),yv(g2jp1(ind))]','r.-','markersize',10);
-  axis image;
-  xlabel('x');
-  ylabel('y');
-  title(sprintf('Edges with length less than %.2f shown in red',dgmin));
+  if(dgmin==0)
+    fprintf('Minimum Dg is %.2f\n',min(Dg));
+  else
+    ind = find(Dg<dgmin);
+    
+    figure(1);
+    hist(Dg,linspace(min(Dg),max(Dg),nbins));
+    xlabel('Voronoi Distance');
+    ylabel('Number of edges');
+    title('Voronoi Distance Histogram');
+    
+    figure(2);
+    clf;
+    hold on;
+    plot(xv,yv,'k.','markersize',1);
+    plot([xv(g2j(ind)),xv(g2jp1(ind))]',...
+         [yv(g2j(ind)),yv(g2jp1(ind))]','r.-','markersize',10);
+    axis image;
+    xlabel('x');
+    ylabel('y');
+    title(sprintf('Edges with length less than %.2f shown in red',dgmin));
+  end
+  
