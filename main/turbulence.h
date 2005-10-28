a7f890b8e6e9ba960fbe317331e3b2baaa87bb49
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2005-10-28 23:44:55 +0000

    Added LBACKGROUND which is the background length scale.  Set to 1e-12 when l[i][k]==0.

--- main/turbulence.h
+++ main/turbulence.h
@@ -2,8 +2,11 @@
  * File: turbulence.h
  * Description: Header file for turbulence.c
  *
- * $Id: turbulence.h,v 1.2 2004-09-16 21:33:01 fringer Exp $
+ * $Id: turbulence.h,v 1.3 2005-10-28 23:44:55 fringer Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/09/16 21:33:01  fringer
+ * Added comm and myproc variables to definition of my25.
+ *
  * Revision 1.1  2004/09/13 04:14:55  fringer
  * Header file for turbulence.c.
  *
@@ -14,6 +17,9 @@
 
 // Von Karman's Constant
 #define KAPPA_VK 0.42
+
+// Background length scale
+#define LBACKGROUND 1e-12
 
 void my25(gridT *grid, physT *phys, propT *prop, REAL **q, REAL **l, REAL **Cn_q, REAL **Cn_l, REAL **nuT, REAL **kappaT, MPI_Comm comm, int myproc);
 
