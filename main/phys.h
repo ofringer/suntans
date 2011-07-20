421a384f7d1865e9cdcfa04f4edc2be8e52c34ef
fringer <fringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2011-07-20 05:15:47 +0000

    - Greatly simplified bottom drag coefficient code by using flux-face height dzf.  This was necessary since face heights for drag using averages on either side of face produced heights that were too large and so were not inducing enough drag. - Changed bottom drag calculation to be Cd*|phys->utmp|*phys->u since this adds more stability because typically |phys->utmp|>|phys->u|. - Added code to output restart data at intervals of ntoutStore

--- main/phys.h
+++ main/phys.h
@@ -130,7 +130,7 @@ typedef struct _propT {
   REAL dt, Cmax, rtime, amp, omega, flux, timescale, theta0, theta, 
     thetaS, thetaB, nu, nu_H, tau_T, z0T, CdT, z0B, CdB, CdW, relax, epsilon, qepsilon, resnorm, 
     dzsmall, beta, kappa_s, kappa_sH, gamma, kappa_T, kappa_TH, Coriolis_f, CmaxU, CmaxW, laxWendroff_Vertical;
-  int ntout, ntprog, nsteps, nstart, n, ntconserve, nonhydrostatic, cgsolver, maxiters, qmaxiters, hprecond, qprecond, volcheck, masscheck,
+  int ntout, ntoutStore, ntprog, nsteps, nstart, n, ntconserve, nonhydrostatic, cgsolver, maxiters, qmaxiters, hprecond, qprecond, volcheck, masscheck,
     nonlinear, newcells, wetdry, sponge_distance, sponge_decay, thetaramptime, readSalinity, readTemperature, turbmodel, 
     TVD, horiTVD, vertTVD, TVDsalt, TVDtemp, TVDturb, laxWendroff, stairstep;
   FILE *FreeSurfaceFID, *HorizontalVelocityFID, *VerticalVelocityFID,
