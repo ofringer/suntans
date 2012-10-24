46d110855ea2a1e9d634fb76f9066bce94eb4547
ofringer <ofringer@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2012-10-24 18:50:38 +0000

    Yi-Ju's Sediment and Wave Code

--- papers/guide/figures/compoundchannel.m
+++
@@ -1,9 +0,0 @@
-
-open('compoundchannel.fig');
-plot([0 0 1 1 1.25 1.25 2.25 2.25],...
-     [0 -.1 -.1 -.5 -.5 -.1 -.1 0],'k-',...
-     [0 2.25],[-.02 -.02],'k-');
-axis image;
-axis off;
-
-print -deps2 compoundchannel
