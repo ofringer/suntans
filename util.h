af61907befea094f1dc7c241dbabc55b23192faa
zyaj <zyaj@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2012-11-10 06:12:38 +0000

    New grid.c for quadgrid

--- util.h
+++ util.h
@@ -22,12 +22,12 @@ enum Type
 };
 
 void Sort(int *a, int *v, int N);
-void ReOrderIntArray(int *a, int *order, int *tmp, int N, int Num);
-void ReOrderRealArray(REAL *a, int *order, REAL *tmp, int N, int Num);
+void ReOrderIntArray(int *a, int *order, int *tmp, int N, int Num, int *nfaces, int *grad, int maxfaces);
+void ReOrderRealArray(REAL *a, int *order, REAL *tmp, int N, int Num, int *nfaces, int *grad, int maxfaces);
 int *ReSize(int *a, int N);
 int IsMember(int i, int *points, int numpoints);
 int FindNearest(int *points, REAL *x, REAL *y, int N, int np, REAL xi, REAL yi);
-void Interp(REAL *x, REAL *y, REAL *z, int N, REAL *xi, REAL *yi, REAL *zi, int Ni);
+void Interp(REAL *x, REAL *y, REAL *z, int N, REAL *xi, REAL *yi, REAL *zi, int Ni, int maxFaces);
 void TriSolve(REAL *a, REAL *b, REAL *c, REAL *d, REAL *u, int N);
 int IsNan(REAL x);
 REAL UpWind(REAL u, REAL dz1, REAL dz2);
