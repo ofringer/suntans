dd05d3d1d1340b9c1ab048c2b09c1f97aa47e1c5
mrayson <mrayson@users.noreply.github.com>
2014-02-04 05:08:33 +0000

    Added two new test cases to the examples: "riverplume" and "heatflux".     1) riverplume - rotating buoyant plume in a box. Demonstrates:     	- How to add river boundaries 	- Test different mesh types: tri, quad, hex, gmsh quad.     2) heatflux - 1D heat flux test case. Demonstrates:     	- How to run the heat flux model. 	- How to download observations and generate a meteorological input netcdf file.

---
+++ examples/heatflux/scripts/nwsobs2suntansmet.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Example of how to get National Weather Service observations, convert them into a
+searchable format, and finally convert to a SUNTANS  meteorological netcdf format
+
+Created on Mon Nov 18 13:50:17 2013
+
+@author: mrayson
+"""
+
+import getNOAAWeatherStation as noaa
+import netcdfio
+from createSunMetFile import interpWeatherStations, write2NC
+import os
+from datetime import datetime,timedelta
+
+###################################################
+# Input variables
+
+# Position of point of interest +/- dx degrees
+lon0 = -94.86367
+lat0 = 29.29517
+utmzone = 15
+
+# Time period to download
+timestart = '20100101'
+timeend = '20120101'
+dt = 1.0/24.0
+
+# Local directory where to store raw NWS-tar.gz data files
+localdir = 'data-files'
+
+# Netcdf file and shapefile containing the data and its location
+ncfile = 'data-files/NCDCNWS_AirObs_20102011.nc'
+shpfile ='data-files/NCDCNWS_AirObs_20102011.shp'
+
+# Database file that stores station metadata
+dbfile = 'data-files/WeatherObs.db'
+
+# Name of the suntans netcdf met file
+metncfile = 'rundata/Galveston_Met.nc'
+###################################################
+
+###
+# Step 1: Download the NWS weather files and convert to a netcdf-4 format
+###
+dx = 0.1
+# Set the date range slightly larger just in case
+tstart = datetime.strptime(timestart,'%Y%m%d')-timedelta(days=30)
+tend = datetime.strptime(timeend,'%Y%m%d')+timedelta(days=30)
+
+latlon = [lon0-dx,lon0+dx,lat0-dx,lat0+dx]
+
+data = noaa.noaaish2nc(latlon,[tstart.year,tend.year],localdir,ncfile,shpfile)
+
+###
+# Step 2: Create a database and insert the netcdf metadata into it
+###
+print 'Creating database: %s'%dbfile
+    
+if os.path.exists(dbfile):
+    print 'Overwriting old file...'
+    os.unlink(dbfile)
+    
+netcdfio.createObsDB(dbfile)
+    
+ncfiles=[ncfile]
+for nc in ncfiles:
+    print 'Inserting metadata from: %s'%nc    
+    netcdfio.netcdfObs2DB(nc,dbfile)
+
+###
+# Step 3: Create a SUNTANS format meteorological input file
+###
+[coords, output, nctime] = interpWeatherStations(latlon,timestart,timeend,dt*24.0,utmzone,dbfile,maxgap=100,showplot=False)
+# Write to the suntans meteorological netcdf format
+write2NC(metncfile,coords,output,nctime)
---
+++ examples/heatflux/scripts/plotTimeSeries.py
@@ -0,0 +1,90 @@
+"""
+Contour plot (z-t) of the heat flux model
+"""
+
+from sunpy import TimeSeries
+import matplotlib.pyplot as plt
+import numpy as np
+
+ncfile = 'data/Heatflux_AVG.nc.0'
+XY = np.array([317646., 3240796.])
+
+# Load the suntans time series object
+TS = TimeSeries(ncfile,XY)
+
+
+# Plot temperature
+plt.figure()
+
+TS['variable']='temp'
+ax1=plt.subplot(311)
+TS.contourf()
+plt.colorbar()
+plt.ylabel('Depth [m]')
+
+# Plot salinity
+TS['variable']='salt'
+ax2=plt.subplot(312)
+TS.contourf()
+plt.colorbar()
+plt.ylabel('Depth [m]')
+
+# Plot the heat flux value
+ax3=plt.subplot(313)
+TS['variable']='Hsw'
+Hsw = TS.data.copy()
+p0 = plt.plot(TS.time,Hsw,color='r')
+
+TS['variable']='Hlw'
+Hlw = TS.data.copy()
+p1 = plt.plot(TS.time,Hlw,color='b')
+
+TS['variable']='Hl'
+Hl = TS.data.copy()
+p2 = plt.plot(TS.time,Hl,color='m')
+
+TS['variable']='Hs'
+Hs = TS.data.copy()
+p3 = plt.plot(TS.time,Hs,color='g')
+plt.xticks(rotation=17)
+plt.ylabel('Heat Flux $[W \ m^{-2}]$')
+
+## Plot the heat flux value
+#ax3=plt.subplot(313)
+#TS['variable']='Hsw'
+#Heat0 = np.zeros_like(TS.data)
+#Heat = Heat0 + TS.data
+#p0 = plt.fill_between(TS.time,Heat,y2=Heat0,color='r',alpha=0.7)
+#
+#TS['variable']='Hlw'
+#Heat0 = Heat.copy()
+#Heat = Heat0 + TS.data
+#p1= plt.fill_between(TS.time,Heat,y2=Heat0,color='b',alpha=0.7)
+#
+#TS['variable']='Hl'
+#Heat0 = Heat.copy()
+#Heat = Heat0 + TS.data
+#p2 = plt.fill_between(TS.time,Heat,y2=Heat0,color='m',alpha=0.7)
+#
+#TS['variable']='Hs'
+#Heat0 = Heat.copy()
+#Heat = Heat0 + TS.data
+#p3 = plt.fill_between(TS.time,Heat,y2=Heat0,color='g',alpha=0.7)
+#plt.xticks(rotation=17)
+#plt.ylabel('Heat Flux $[W \ m^{-2}]$')
+
+
+# legend
+plt.legend((p0[0],p1[0],p2[0],p3[0]),('$H_{sw}$','$H_{lw}$','$H_L$','$H_S$'))
+
+# Set the axis width
+bbox2 = ax2.get_position().bounds
+bbox3 = list(ax3.get_position().bounds)
+bbox3[2]=bbox2[2]
+ax3.set_position(bbox3)
+
+ax1.set_xticklabels([])
+ax2.set_xticklabels([])
+
+plt.ylim([-1e3,1e3])
+plt.show()
