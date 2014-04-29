eedb0dd829600435ee3e049af2567fe8b6b19ed2
mrayson <mrayson@e0f04c94-b611-4bbd-8b54-73c08bc3ee87>
2014-04-29 22:09:38 +0000

    Added Godin filter to the timeseries object. Also, made the running mean and despiking methods more efficient by using strided arrays.

--- python/Utils/timeseries.py
+++ python/Utils/timeseries.py
@@ -151,8 +151,75 @@ class timeseries(object):
         return np.swapaxes(ytmp,axis,-1)
         #return signal.filtfilt(b, a, self.y, axis=axis)
 
+    def godinfilt(self,filtwidths=[24,25]):
+        """
+        Apply the successive Godin-type filter to the data set
         
-    def interp(self,timein,method='linear',timeformat='%Y%m%d.%H%M%S',axis=-1):
+        The filter widths are defined by filtwidths (hours).
+        
+        For a 24-25-24 hour filter set filtwidths=[24,25] (default).
+        """
+        
+        if self.isequal==False or self.dt != 3600.:
+            # Puts the data onto an hourly matrix
+            self._evenly_dist_data(3600.)
+            
+        ymean = self.running_mean(windowlength=24*3600.)
+        self.y = ymean
+        ymean = self.running_mean(windowlength=25*3600.)
+        self.y = ymean
+        ymean = self.running_mean(windowlength=24*3600.)
+        self.y = ymean
+        
+            
+#        # Calculate the weights
+#        dt = self.dt/3600. # time step in hours
+#        nbands = [int(fw/dt) for fw in filtwidths]
+#        nb = map(float,nbands)
+#        window = nbands[0]*2+nbands[1]-2 # number of points in the window
+#        
+#        weights = np.zeros((window,))
+#        weights[0:nbands[0]]=1/nb[0]
+#        weights[nbands[0]-1:nbands[1]+nbands[0]]+=1/nb[1]
+#        weights[nbands[0]+nbands[1]-1::]+=1/nb[0]
+#        weights/=weights.sum()
+#        
+#        # Emery and Thompson weights (assumes hourly data)
+##        weights = np.zeros((36,))
+##        for k in range(12):
+##            weights[k] = 1./28800 * (1200 - (12-k)*(13-k) - (12+k)*(13+k) )
+##            
+##        for k in range(12,36):
+##            weights[k] = 1./28800 * (36-k)*(37-k)
+##        weights = np.hstack((weights[::-1],weights[1::]))
+#        
+#        
+#        # masked values are included in the sum so we
+#        # will just zero them
+#        mask = self.y.mask.copy()
+#        self.y[self.y.mask]=0.
+#        
+#        # Apply the weights along the last axis of a strided array
+#        ytmp = self.y.copy()
+#        ytmp = self._window_matrix(ytmp,window)
+#        ytmp *=weights
+#        ytmp = ytmp.sum(axis=-1)       
+#        
+#        # This result needs to be normalized to account for missing data,
+#        # this is the same as calculating different weights for each section
+#        ntmp= np.ones_like(self.y)
+#        ntmp[mask] = 0.
+#        norm = self._window_matrix(ntmp,window)
+#        norm*=weights
+#        norm = norm.sum(axis=-1)
+#        
+#        ytmp/=norm
+#        
+#        # Update the y variable
+#        self.y = self._update_windowed_data(ytmp,window)
+#        self.y.mask=mask
+            
+    def interp(self,timein,method='linear',timeformat='%Y%m%d.%H%M%S',axis=0):
         """
         Interpolate the data onto an equally spaced vector
         
@@ -180,9 +247,10 @@ class timeseries(object):
         if self.ndim > 1:
             # Interpolate multidimensional arrays without a mask
             F = interpolate.interp1d(self.tsec,self.y,kind=method,axis=axis,\
-            bounds_error=False,fill_value=0)
+                bounds_error=False,fill_value=0)
         else:
-            mask = np.isnan(self.y) == False
+            #mask = np.isnan(self.y) == False
+            mask = ~self.y.mask
             F = interpolate.interp1d(self.tsec[mask],self.y[mask],kind=method,axis=axis,\
             bounds_error=False,fill_value=0)
         
@@ -269,55 +337,79 @@ class timeseries(object):
             
         return tmid, amp, phs
             
-    def running_mean(self,windowlength=3*86400.0,overlap=12*3600.0,\
-        plot=True,calcrms=True):
+#    def running_mean(self,windowlength=3*86400.0,overlap=12*3600.0,\
+#        plot=True,calcrms=True):
+    def running_mean(self,windowlength=3*86400.0):
         """
         Running mean and RMS of the time series
         
         windowlength - length of each time window [seconds]
         overlap - overlap between windows [seconds]
         """
+        mask = self.y.mask.copy()
+        self.y[self.y.mask]=0.
+        self.y.mask=mask
         
-        # Make sure that omega is a list
-
-        pt1,pt2 = window_index_time(self.t,windowlength,overlap)
-        npt = len(pt1)
-        tmid = []
-        ymean = np.zeros((npt,))
-        yrms = np.zeros((npt,))
-
-        ii=-1
-        for t1,t2 in zip(pt1,pt2):
-            ii+=1
-
-            ymean[ii] = self.y[t1:t2].mean()
-            if calcrms:
-                yrms[ii] = crms(self.tsec[t1:t2],self.y[t1:t2]-ymean[ii])
-            
-            # Return the mid time point
-            ind = int(np.floor(t1 + (t2-t1)/2))
-            tmid.append(self.t[ind])
-   
-        tmid = np.asarray(tmid)
+        windowsize = np.floor(windowlength/self.dt)
+        ytmp = self.y.copy()
+        ytmp = self._window_matrix(ytmp,windowsize)
         
-        if plot:
-            plt.subplot(211)
-            self.plot()
-            plt.plot(tmid,ymean,'r')
-            plt.fill_between(tmid,ymean-yrms,y2=ymean+yrms,color=[0.5,0.5,0.5],alpha=0.5)
-            plt.legend(('Original Signal','Mean','$\pm$ RMS'))
-            
-            ax=plt.subplot(212)
-            plt.fill_between(tmid,yrms,alpha=0.7)
-            plt.xticks(rotation=17)
-            plt.ylabel('RMS')
-            ax.set_xlim([self.t[0],self.t[-1]])
-            
-            
-        return tmid, ymean, yrms     
+        weights = 1./windowsize * np.ones((windowsize,))
+        ytmp2 = np.sum(ytmp*weights,axis=-1)
+        
+        # This result needs to be normalized to account for missing data,
+        # this is the same as calculating different weights for each section
+        ntmp= np.ones_like(self.y)
+        ntmp[mask] = 0.
+        norm = self._window_matrix(ntmp,windowsize)
+        #norm*= weights
+        norm = norm.sum(axis=-1)
+        norm /= windowsize
+        
+        ytmp2/=norm
+                
+        return self._update_windowed_data(ytmp2,windowsize)
+        
+
+
+#        pt1,pt2 = window_index_time(self.t,windowlength,overlap)
+#        npt = len(pt1)
+#        tmid = []
+#        ymean = np.zeros((npt,))
+#        yrms = np.zeros((npt,))
+#
+#        ii=-1
+#        for t1,t2 in zip(pt1,pt2):
+#            ii+=1
+#
+#            ymean[ii] = self.y[t1:t2].mean()
+#            if calcrms:
+#                yrms[ii] = crms(self.tsec[t1:t2],self.y[t1:t2]-ymean[ii])
+#            
+#            # Return the mid time point
+#            ind = int(np.floor(t1 + (t2-t1)/2))
+#            tmid.append(self.t[ind])
+#   
+#        tmid = np.asarray(tmid)
+#        
+#        if plot:
+#            plt.subplot(211)
+#            self.plot()
+#            plt.plot(tmid,ymean,'r')
+#            plt.fill_between(tmid,ymean-yrms,y2=ymean+yrms,color=[0.5,0.5,0.5],alpha=0.5)
+#            plt.legend(('Original Signal','Mean','$\pm$ RMS'))
+#            
+#            ax=plt.subplot(212)
+#            plt.fill_between(tmid,yrms,alpha=0.7)
+#            plt.xticks(rotation=17)
+#            plt.ylabel('RMS')
+#            ax.set_xlim([self.t[0],self.t[-1]])
+#            
+#            
+#        return tmid, ymean, yrms     
         
     def despike(self,nstd=4.,windowlength=3*86400.0,overlap=12*3600.0,\
-        upper=np.inf,lower=-np.inf):
+        upper=np.inf,lower=-np.inf,maxdiff=np.inf,fillval=0.):
         """
         Despike time series by replacing any values greater than nstd*std.dev with the
         median of a running window. 
@@ -327,42 +419,58 @@ class timeseries(object):
         overlap - overlap between windows [seconds]
         lower - lower value bound
         upper - upper value bound
+        maxdiff - maximum difference between two points
         """
+#        if self.isequal==False:
+#            self._evenly_dist_data()
+            
+        nbad = 0
+        
+        # Now check the maximum difference
+        ydiff = np.zeros_like(self.y)
+        ydiff[1::] = np.abs(self.y[1::]-self.y[0:-1])
+        ind = ydiff>=maxdiff        
+        #self.y[ind]=fillval
+        self.y.mask[ind] = True # Mask needs to be set after values are prescribed
+        nbad += np.sum(ind)
+        
+        # First mask any NaN and values outside of bounds
+        ind = operator.or_(self.y<=lower,self.y>=upper)
+        #self.y[ind]=fillval
+        self.y.mask[ind] = True
+        nbad += np.sum(ind)
+        
+        ind =  np.isnan(self.y)
+        #self.y[ind]=fillval
+        self.y.mask[ind] = True
+        
+        nbad += np.sum(ind)
+        
+        # Now calculate the moving median and standard deviation
+        windowsize = np.floor(windowlength/self.dt)
+        ytmp = self.y.copy()
+        ytmp = self._window_matrix(ytmp,windowsize)
+        
+        ytmp2 = np.mean(ytmp,axis=-1)
+        ymean = self._update_windowed_data(ytmp2,windowsize)
+        
+        #ytmp2= np.std(ytmp,axis=-1)
+        ytmp2 = np.apply_along_axis(np.std,-1,ytmp2)
+        ystd = self._update_windowed_data(ytmp2,windowsize)
+        
+        # Mask values outsize of the
+        ind = operator.or_(self.y >= ymean + nstd*ystd,\
+                self.y <= ymean - nstd*ystd)
+        
+        #self.y[ind] = ymedian[ind]
+        self.y.mask[ind] = True
+        
+        nbad += np.sum(ind)
+        
+        if self.VERBOSE:
+            print 'Despiked %d points'%nbad
         
         
-        pt1,pt2 = window_index_time(self.t,windowlength,overlap)
-
-        for t1,t2 in zip(pt1,pt2):
-            
-            if t1==t2:
-                continue
-            
-            ytmp = self.y[t1:t2]
-           
-            # Mask any nan's and values outside of the bounds
-            ind = operator.or_(ytmp<=lower,ytmp>=upper)
-            ytmp.mask[ind] = True
-            ind2 =  np.isnan(ytmp)
-            ytmp.mask[ind2] = True
-
-
-            # Now mask any values outside of nstd away from the median
-            ymedian = np.mean(ytmp)
-            ystd = np.std(ytmp)
-            #print ymedian, ytmp.mean(), ystd, type(ymedian)
-            #if type(ymedian) == np.ma.core.MaskedConstant:
-            #    pdb.set_trace()
-            if ~np.any(ytmp.mask==False): # all points are masked
-                self.y[t1:t2]=ytmp.copy()               
-                continue
-
-            ind3 = operator.or_(ytmp >= ymedian + nstd*ystd,\
-                ytmp <= ymedian - nstd*ystd)
-            ytmp.mask[ind3] = True
-            
-            self.y[t1:t2]=ytmp.copy()
-
- 
 #        pt1,pt2 = window_index_time(self.t,windowlength,overlap)
 #
 #        for t1,t2 in zip(pt1,pt2):
@@ -372,28 +480,29 @@ class timeseries(object):
 #            
 #            ytmp = self.y[t1:t2]
 #           
-#            # only calculate the median with values inside of the range
-#            ind = operator.and_(ytmp>=lower,ytmp<=upper)
-#            ind2 = operator.and_(ind, ~np.isnan(ytmp))
-#            
-#            if not any(ind2):
-#                ytmp[:] = lower
-#                self.y[t1:t2]=ytmp.copy()
-#                continue
-#            
-#            ymedian = np.median(ytmp[ind2])
-#            ystd = np.std(ytmp[ind2])
-#            if np.isnan(ymedian):
-#                print t1,t2
-#            
-#            ytmp[ytmp >= ymedian + nstd*ystd] = ymedian
-#            ytmp[ytmp <= ymedian - nstd*ystd] = ymedian
-#            
-#            if ymedian>upper or ymedian < lower:
-#                print 'Warning median value outside of bounds range...'
+#            # Mask any nan's and values outside of the bounds
+#            ind = operator.or_(ytmp<=lower,ytmp>=upper)
+#            ytmp.mask[ind] = True
+#            ind2 =  np.isnan(ytmp)
+#            ytmp.mask[ind2] = True
 #
+#
+#            # Now mask any values outside of nstd away from the median
+#            ymedian = np.mean(ytmp)
+#            ystd = np.std(ytmp)
+#            #print ymedian, ytmp.mean(), ystd, type(ymedian)
+#            #if type(ymedian) == np.ma.core.MaskedConstant:
+#            #    pdb.set_trace()
+#            if ~np.any(ytmp.mask==False): # all points are masked
+#                self.y[t1:t2]=ytmp.copy()               
+#                continue
+#
+#            ind3 = operator.or_(ytmp >= ymedian + nstd*ystd,\
+#                ytmp <= ymedian - nstd*ystd)
+#            ytmp.mask[ind3] = True
+#            
 #            self.y[t1:t2]=ytmp.copy()
-            
+
         
     def plot(self,angle=17,**kwargs):
         """
@@ -426,6 +535,12 @@ class timeseries(object):
             
         f.close()
 
+    def copy(self):
+        """
+        Make a copy of the time-series object in memory
+        """
+        from copy import deepcopy
+        return deepcopy(self)
         
     def _checkDT(self):
         """
@@ -444,16 +559,79 @@ class timeseries(object):
             self.dt = dt[1]
         except:
             self.dt = 0.0
+            
+    def _evenly_dist_data(self,dt):
+        """
+        Distribute the data onto an evenly spaced array
+        
+        No interpolation is performed
+        """
+        if self.VERBOSE:
+            print 'inserting the data into an equally-spaced time vector (dt = %f s).'%self.dt
+    
+        t = self.tsec - self.tsec[0]
+        # Put the data onto an evenly spaced, masked array
+        tout = np.arange(t[0],t[-1]+dt,dt)
+        
+        tind = np.searchsorted(tout,t)
+        
+        yout = np.ma.MaskedArray(np.zeros_like(tout),mask=True)
+
+        yout[tind] = self.y
+        
+        def updatetime(tsec):
+            return timedelta(seconds=tsec) + self.t[0]
+            
+        self.t = map(updatetime,tout)
+        self.y = yout
+        
+        self.tsec = tout
+        
+        self.ny = np.size(self.y)
+        
+        self.isequal = True
+        
+        self.dt = dt
+        
+    def _window_matrix(self,y,windowsize):
+        """
+        Returns the matrix as a strided array so that 'rolling' operations can
+        be performed along the last axis
+        """
+        shape = y.shape[:-1] + (y.shape[-1] - windowsize + 1, windowsize)
+        strides = y.strides + (y.strides[-1],)
+        
+        # The masked values get 
+        return np.lib.stride_tricks.as_strided(y, shape=shape, strides=strides)
+    
+    def _update_windowed_data(self,ytmp,windowsize):
+        """
+        Re-inserts data that has been windowed into an array
+        that is the same size as the original time series
+        """
+        y = np.zeros_like(self.y)
+        indent = (windowsize-np.mod(windowsize,2))/2
+        
+        if np.mod(windowsize,2)==1:
+            y[indent:-indent]=ytmp
+        else:
+            y[indent-1:-indent]=ytmp
+        
+        y = np.ma.MaskedArray(y,mask=self.y.mask)
+        y.mask[0:indent]=True
+        y.mask[-indent:]=True
+        
+        return y
 
 class ModVsObs(object):
     """
     Class for handling and comparing two time series i.e. model vs observations
     """
 
-    units=''
-    long_name=''
-    stationid = ''
-    varname = ''
+    units=' '
+    long_name=' '
+    stationid = ' '
+    varname = ' '
     Z=0.0
 
     def __init__(self,tmod,ymod,tobs,yobs,**kwargs):
@@ -625,7 +803,28 @@ class ModVsObs(object):
         else:
             return Cxy ,tau
  
+    def csd(self, plot=True,nbandavg=1,**kwargs):
+        """
+        Cross spectral density
+        
+        nbandavg = Number of fft bins to average
+        """
+        
+        if self.isequal==False and self.VERBOSE:
+            print 'Warning - time series is unequally spaced consider using lomb-scargle fft'
+        
 
+        NFFT = int(2**(np.floor(np.log2(self.ny/nbandavg)))) # Nearest power of 2 to length of data
+            
+        Pyy,frq = mlab.csd(self.TSobs.y-self.TSobs.y.mean(),Fs=2*np.pi/self.dt,NFFT=NFFT,window=mlab.window_hanning,scale_by_freq=True)
+        
+        if plot:
+            plt.loglog(frq,Pyy,**kwargs)
+            plt.xlabel('Freq. [$cycles s^{-1}$]')
+            plt.ylabel('PSD')
+            plt.grid(b=1)
+        
+        return Pyy, frq
 
 
 def harmonic_fit(t,X,frq,mask=None,axis=0,phsbase=None):
@@ -788,7 +987,7 @@ def loadDBstation(dbfile,stationID,varname,timeinfo=None,filttype=None,cutoff=36
         
     if not filttype==None:
         print '%s-pass filtering output data. Cutoff period = %f [s].'%(filttype,cutoff)
-        yfilt = ts.filt(cutoff,btype=filttype,axis=-1)
+        yfilt = ts.filt(cutoff,btype=filttype,axis=0)
         ts.y = yfilt.copy()
     
     if output_meta:
